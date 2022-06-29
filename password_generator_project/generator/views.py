247a418acd892838c71831123fba028ca2533057
AlexKitIt <alexkitit8@gmail.com>
2022-06-29 22:43:54 +0400

    Django создание генератора паролей

---
+++ password_generator_project/generator/views.py
@@ -0,0 +1,23 @@
+import random
+
+from django.shortcuts import render
+
+
+def home(request):
+    lst = list(range(6, 15))
+    return render(request, "generator/home.html", {'lst': lst})
+
+
+def password(request):
+    psw = ''
+    char = [chr(i) for i in range(97, 123)]
+    if request.GET.get('uppercase'):
+        char.extend([chr(i) for i in range(65, 91)])
+    if request.GET.get('numbers'):
+        char.extend([str(i) for i in range(10)])
+    if request.GET.get('special'):
+        char.extend(list(r"-=+!@#$%^&*()\|/"))
+    length = int(request.GET.get('length'))
+    for i in range(length):
+        psw += random.choice(char)
+    return render(request, "generator/password.html", {'password': psw})
